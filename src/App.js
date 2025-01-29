2e8d6806a18e7ec90a5de8c122750ff7eaf4ff08
Sohel Datta <soheldatta17@gmail.com>
2025-01-29 13:28:16 +0530

    .

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
       return;
     }
 
-    const response = await fetch('http://127.0.0.1:5000/plot', {
+    const response = await fetch('https://backend-plotter.vercel.app/plot', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
